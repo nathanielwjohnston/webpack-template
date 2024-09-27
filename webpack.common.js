a0e77883223caffb17d940a108c77b18bfb5ad8f
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-09-27 11:22:26 +0100

    Set up HTML bundling and basic file template

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,11 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
